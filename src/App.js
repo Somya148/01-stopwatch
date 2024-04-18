f290140e4b80f2ced0038ee8341d337ae7a76811
Somya148 <ssomya395@gmail.com>
2024-04-18 13:01:12 +0530

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -24,48 +24,47 @@ function App() {
   return (
     <div className="bg-black  h-screen flex items-center justify-center text-white">
 
-      <div className="h-[40rem]  w-[19rem] rounded-2xl border-4 border-gray-500 flex justify-center align-center items-center">
 
-        <div className="h-[39rem]  w-[18rem] rounded-2xl border-2 border-gray-500 ">
+      <div className="h-[600px] w-80 rounded-2xl border-2 border-gray-500 ">
 
-          <div className="m-5 text-5xl">
-            <h1 className="text-yellow-400 ">Measure </h1>
-            <h1><span className="italic font-thin">time</span> of your</h1>
-            <h1> Activities.</h1>
+        <div className="m-5 text-5xl">
+          <h1 className="text-yellow-400 ">Measure </h1>
+          <h1><span className="italic font-thin">time</span> of your</h1>
+          <h1> Activities.</h1>
+        </div>
+
+        <div className="m-5">
+          <h1><span className="text-yellow-400">01</span> - StopWatch</h1>
+          <p className="text-gray-700 italic font-semibold pt-3 pb-7">Keep time on your side with a stopwatch.</p>
+        </div>
+
+
+        <div className="rounded-full border-4 border-yellow-400 w-44 h-44 m-auto flex justify-center align-center">
+          <div className="rounded-full border-2 border-yellow-400 w-40 h-40 m-auto pl-8 pt-14 font-thin text-2xl">
+            <span>{("0" + Math.floor(time / 60000 % 60)).slice(-2)}:</span>
+            <span>{("0" + Math.floor(time / 1000 % 60)).slice(-2)}:</span>
+            <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
           </div>
-
-          <div className="m-5">
-            <h1><span className="text-yellow-400">01</span> - StopWatch</h1>
-            <p className="text-gray-700 italic font-semibold pt-3 pb-7">Keep time on your side with a stopwatch.</p>
-          </div>
+        </div>
 
 
-          <div className="rounded-full border-4 border-yellow-400 w-44 h-44 m-auto flex justify-center align-center">
-            <div className="rounded-full border-2 border-yellow-400 w-40 h-40 m-auto pl-8 pt-14 font-thin text-2xl">
-              <span>{("0" + Math.floor(time / 60000 % 60)).slice(-2)}:</span>
-              <span>{("0" + Math.floor(time / 1000 % 60)).slice(-2)}:</span>
-              <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-            </div>
-          </div>
-
-
-          <div className="flex justify-center align-center mt-5">
+        <div className="flex justify-center align-center mt-5">
 
 
 
-            {running ? (
-              <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-4 rounded" onClick={() => setRunning(false)}>Stop</button>
-            ) : (
-              <button className="bg-yellow-400 hover:bg-yellow-600 text-black font-semibold py-1 px-4 rounded" onClick={() => setRunning(true)}>Start</button>
-            )}
-            <button className="bg-gray-700  hover:bg-gray-800 text-white  font-semibold py-1 px-4 rounded ml-4" onClick={() => setTime(0)}>Reset</button>
-
-          </div>
+          {running ? (
+            <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-4 rounded" onClick={() => setRunning(false)}>Stop</button>
+          ) : (
+            <button className="bg-yellow-400 hover:bg-yellow-600 text-black font-semibold py-1 px-4 rounded" onClick={() => setRunning(true)}>Start</button>
+          )}
+          <button className="bg-gray-700  hover:bg-gray-800 text-white  font-semibold py-1 px-4 rounded ml-4" onClick={() => setTime(0)}>Reset</button>
 
         </div>
 
       </div>
+
     </div>
+
   );
 }
 
